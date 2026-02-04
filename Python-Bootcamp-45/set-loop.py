b6d1085b826fede0c394c06dcd2c19115dfd5c8a
d1yordev <diorkh07@gmail.com>
2026-02-04 23:01:48 +0800

    Leraned set-Loop and practiced

---
+++ Python-Bootcamp-45/set-loop.py
@@ -0,0 +1,13 @@
+hobbies = [
+    "football", "music", "reading", "music",
+    "coding", "football", "gaming", "coding"
+]
+
+print(f"Unique hobbies: {len(set(hobbies))}")
+print(f"Unique hobbies: {set(hobbies)}")
+
+for hobbie in hobbies:
+    print(hobbie)
+
+for hobbie in hobbies:
+    if
