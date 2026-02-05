cffe34c58bcec180d86fae0a665e546f8052b2db
d1yordev <diorkh07@gmail.com>
2026-02-05 17:19:26 +0800

    Leraned set-Loop and practiced

--- Python-Bootcamp-45/set-loop.py
+++ Python-Bootcamp-45/set-loop.py
@@ -1,13 +1,74 @@
 hobbies = [
-    "football", "music", "reading", "music",
-    "coding", "football", "gaming", "coding"
+    "football", "music", "reading", "reading", "reading", "music","music",
+    "coding", "cleaning", "hokkey","hokkey","hokkey","hokkey","hokkey", "panda",
+    "panda", "panda", "panda", "coding",
+    "football", "gaming","gaming","gaming","gaming", "coding", "cleaning","cleaning","cleaning",
+    "selling","selling","selling", "enjoying","enjoying","enjoying","enjoying","enjoying", "kidding", "joking","kidding", "joking","kidding", "joking"
 ]
 
-print(f"Unique hobbies: {len(set(hobbies))}")
-print(f"Unique hobbies: {set(hobbies)}")
+unique_hobbies = set(hobbies)
 
-for hobbie in hobbies:
-    print(hobbie)
+# NECHTA UNIQUE HOBBY BOR
+print(f"\nTotal unique hobbies: {len(unique_hobbies)}")
 
-for hobbie in hobbies:
-    if
+# UNIQUE HOBBIES (SET)
+print("Unique hobbies:")
+for hobby in unique_hobbies:
+    print("-", hobby)
+
+# HAR BIR HOBBY NECHA MARTA QAYTARILGAN
+print("\nHobby counts:")
+
+for hobby in unique_hobbies:
+    count = 0
+    for h in hobbies:
+        if h == hobby:
+            count += 1
+    print(f"{hobby}: {count}")
+
+# ENG KO‘P UCHRAGAN HOBBY
+
+most_common = None
+max_count = 0
+
+for hobby in unique_hobbies:
+    count = 0
+    for h in hobbies:
+        if h == hobby:
+            count += 1
+
+    if count > max_count:
+        max_count = count
+        most_common = hobby
+
+print(f"\nMost common hobby: {most_common} ({max_count} times)")
+
+min_count = max_count      # katta son bilan boshlanadi
+least_common = None
+
+for hobby in unique_hobbies:
+    count = 0          # HAR SAFAR YANGI
+
+    for h in hobbies:
+        if h == hobby:
+            count += 1
+
+    if count < min_count:
+        min_count = count
+        least_common = hobby
+
+print(f"Least common hobby: {least_common} ({min_count} times)")
+
+
+# FAQAT BIR MARTA UCHRAGAN HOBBYLAR
+
+print("\nHobbies used only once:")
+
+for hobby in unique_hobbies:
+    count = 0
+    for h in hobbies:
+        if h == hobby:
+            count += 1
+
+    if count == 1:
+        print("-", hobby)
---
+++ test.py
@@ -0,0 +1,26 @@
+data = [
+    "football", "music", "reading", "music",
+    "coding","cleaning", "hokkey", "panda",
+    "panda", "panda", "panda","coding",
+    "football", "gaming", "coding", "cleaning",
+    "selling", "enjoying", "kidding", "joking"
+]
+print(f"Length of data: {len(data)}")
+
+for d in data:
+    print("-", d)
+
+
+unique_data = set(data)
+print(f"Unique data: {len(set(data))}")
+
+for d in unique_data:
+    print("-", d)
+
+print("Hooby count:")
+for x in unique_data:
+    count =0
+    for y in data:
+        if x == y:
+            count += 1
+    print(f"{x} : {count}")
