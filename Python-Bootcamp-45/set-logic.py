5191f0baa1ae02d5a9631741c91f622f898b6853
d1yordev <diorkh07@gmail.com>
2026-02-08 23:18:04 +0800

    Set logic => Union , Intersection, Difference and Experiment

--- Python-Bootcamp-45/set-logic.py
+++ Python-Bootcamp-45/set-logic.py
@@ -1,9 +1,9 @@
 user1 = {"football", "music", "coding", "reading"}
 user2 = {"music", "coding", "gaming"}
 
-common = user1 | user2
+common = user1 & user2
 
-all_interests = user1 & user2
+all_interests = user1 | user2
 
 only_user1 = user1 - user2
 
@@ -13,3 +13,32 @@ print("Common:", common)
 print("All:", all_interests)
 print("Only user1:", only_user1)
 print("Only user2:", only_user2)
+
+# Task-2
+
+Sali = {"football", "music", "coding", "reading"}
+Vali = {"music", "coding", "gaming"}
+Tali = {"coding", "swimming", "cleaning"}
+
+all_interests2 = Sali & Vali & Tali
+
+print(f"Umumiy Qiziqishlar: {all_interests2}")
+
+# only Sali
+
+only_Sali = Sali - Vali - Tali
+print(f"Only Sali Qiziqishlar: {only_Sali}")
+
+# only Vali
+
+only_vali = Vali - Sali - Tali
+print(f"Only Vali Qiziqishlar: {only_vali}")
+
+# only Tali
+
+only_Tali = Tali - Sali - Vali
+print(f"Only Tali Qiziqishlar: {only_Tali}")
+
+all_interests = Sali | Vali | Tali
+
+print(f"All Qiziqishlar: {all_interests}")
