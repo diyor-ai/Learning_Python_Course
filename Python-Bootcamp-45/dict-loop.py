b0c68da3d7ddd82483f657f7271505c84c8f3c9e
d1yordev <diorkh07@gmail.com>
2026-02-11 23:19:24 +0800

    Learn Dict + Loop and experiance with project

---
+++ Python-Bootcamp-45/dict-loop.py
@@ -0,0 +1,29 @@
+# Project => User Info Printer
+
+users = {
+    "user1": {
+        "name": "Sali",
+        "age": 22,
+        "skills": ["python", "sql", "ai"],
+        "active": True
+    },
+    "user2": {
+        "name": "Vali",
+        "age": 19,
+        "skills": ["html", "css"],
+        "active": False
+    }
+}
+
+for key, value in users.items():
+    print(f"USER: {key}")
+    print(f"NAME: {value['name']}")
+    print(f"AGE: {value['age']}")
+    print(f"SKILLS: ")
+    for skill in value['skills']:
+        print(f" - {skill}")
+
+    if value['active'] is True:
+        print(f"Status: Active")
+    else:
+        print(f"Status: Inactive")
