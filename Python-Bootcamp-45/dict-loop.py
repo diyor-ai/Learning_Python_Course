0caaea87b8ec0a5898c6ab2c88106197dad68e04
d1yordev <diorkh07@gmail.com>
2026-02-12 10:28:15 +0800

    Dict + Loop and experiance with project

--- Python-Bootcamp-45/dict-loop.py
+++ Python-Bootcamp-45/dict-loop.py
@@ -28,8 +28,21 @@ for key, value in users.items():
     else:
         print(f"Status: Inactive")
 
+#Second version
+print("-" * 64)
 
-    if value['active'] is True:
-        print(f"Status: Active")
-    else:
-        print(f"Status: Inactive")
+
+for user_id, info in users.items():
+    print(f"\nUSER: {user_id}")
+    for key, value in info.items():
+        if isinstance(value, list):
+            print(f"{key.upper()}:")
+            for item in value:
+                print(f" - {item}")
+        elif isinstance(value, bool):
+            status = "Active" if value else "Inactive"
+            print(f"Status: {status}")
+        else:
+            print(f"{key.upper()}: {value}")
+
+    print("-" * 16)
