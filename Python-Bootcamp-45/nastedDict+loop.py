2e1e9b3fdb628ad9c9a371be6ac345697b02f1a8
d1yordev <diorkh07@gmail.com>
2026-02-15 01:28:37 +0800

    Dict + Loop and experiance with project

--- Python-Bootcamp-45/nastedDict+loop.py
+++ Python-Bootcamp-45/nastedDict+loop.py
@@ -9,16 +9,22 @@ store = {
 }
 
 total_stock = 0
-for key, value in store.items():
-    stock = value['stock']
-    for val in value.items():
-        print(stock)
+for stocks in store.values():
+    for value in stocks.values():
+        total_stock += value["stock"]
+print(f"Total stock: {total_stock}")
+
+# find most expensive product
+
+for value in store.values():
+    for x in value.values():
+        max_value = x["price"]
+        if x["price"] > max_value:
+            max_value = x["price"]
+    print(f"Max price: {max_value}")
 
 
-university = {
-    "CS": {
-        "year1": {
-            "students": 50
-        }
-    }
-}
+for stocks in store.values():
+    for key, value in stocks.items():
+        if value["stock"] > 5:
+            print(f"{key}: {value['stock']}")
